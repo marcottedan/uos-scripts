2e58d2c94116881829c79d52866c5e2f65373993
marcottedan <marcottedan@gmail.com>
2025-05-31 09:10:52 -0400

    Use MergeOr lib

--- AutoMining.lua
+++ AutoMining.lua
@@ -1,8 +1,10 @@
 local G = require("Data/Profiles/Scripts/Lib/Global")
 local printItemFn = require("Data/Profiles/Scripts/Lib/Print")
 local getOreFn = require("Data/Profiles/Scripts/Lib/Ore")
+local mergeOreFn = require("Data/Profiles/Scripts/Lib/MergeOre")
 local moveItemLoopFn = require("Data/Profiles/Scripts/Lib/MoveItemLoop")
 local ensureToolFn = require("Data/Profiles/Scripts/Lib/EnsureTool")
+
 
 local pickaxeGraphicId = 0x0E86 -- Tool identifier
 local waitTime = 500
@@ -32,30 +34,6 @@ end
 
 local function toString(b)
     return b and "true" or "false"
-end
-
-function mergeOres()
-
-    local huesToSortedOres = getOreFn()
-
-    for hue, sortedOres in pairs(huesToSortedOres) do
-        -- Loop from the heaviest index down to the 2nd-lightest
-        for i = #sortedOres, 2, -1 do
-
-            local heavierOre = sortedOres[i]
-            local lighterOre = sortedOres[i - 1]
-
-            Player.UseObject(heavierOre.Serial)
-            if Targeting.WaitForTarget(waitTime) then
-                Messages.Overhead("Merging " .. heavierOre.Amount .. " choks into " .. lighterOre.Amount .. " pile.", Player.Serial)
-                Targeting.Target(lighterOre.Serial)
-                Pause(waitTime)
-            end
-            -- Reset array since merging 2 items creates a new SerialId
-            huesToSortedOres = getOreFn()
-        end
-    end
-    return getOreFn()
 end
 
 -- Main function
@@ -122,8 +100,8 @@ function main()
 
     -- Merged them to 1 slot per Hue, prioritizing ground last
     -- Do it twice since server desync can happen
-    mergeOres()
-    local huesToSortedOres = mergeOres()
+    mergeOreFn()
+    local huesToSortedOres = mergeOreFn()
 
     -- Find the only Iron Ore in the piles
     local ironOre = nil
---
+++ AutoSmelt.lua
@@ -0,0 +1,3 @@
+local G = require("Data/Profiles/Scripts/Lib/Global")
+local printItemFn = require("Data/Profiles/Scripts/Lib/Print")
+local getOreFn = require("Data/Profiles/Scripts/Lib/Ore")
