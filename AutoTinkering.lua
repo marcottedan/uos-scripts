61013f454224329ef6539093a445a63623b9df1c
marcottedan <marcottedan@gmail.com>
2025-05-31 20:23:48 -0400

    Add AutoTinkering script

--- AutoTinkering.lua
+++ AutoTinkering.lua
@@ -6,8 +6,6 @@ local itemTypes = require("Data/Profiles/Scripts/Lib/ItemCategory")
 
 -- Tinker's Tool : 7864
 
-
-
 function findTool()
     -- Find an item to use
     tinkerTool = Items.FindByType(7864)
@@ -24,7 +22,6 @@ end
 function makeLast()
     -- Wait maximum 2 seconds for the gump to open
     if Gumps.WaitForGump(G.MakeLast, 2000) then
-        Messages.Print('Gump opened!')
         Pause(1400)
         -- Sends a button click to the gump
         Gumps.PressButton(G.MakeLast, 21)
