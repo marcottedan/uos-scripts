a68713ccededc7db0999db2b6c38a4837cd66246
marcottedan <marcottedan@gmail.com>
2025-05-31 22:29:16 -0400

    Add Drop Crap script to drop.. tinkering crap...

---
+++ DropCrap.lua
@@ -0,0 +1,33 @@
+local G = require("Data/Profiles/Scripts/Lib/Global")
+local printItemFn = require("Data/Profiles/Scripts/Lib/Print")
+local mergeOreFn = require("Data/Profiles/Scripts/Lib/MergeOre")
+local getOreFn = require("Data/Profiles/Scripts/Lib/Ore")
+local itemTypes = require("Data/Profiles/Scripts/Lib/ItemCategory")
+
+-- Barrel Tap : 4100
+
+function dropCrap()
+
+    craps = Items.FindByFilter({ onground = false, graphics = { 4100 } })
+    for _, crap in ipairs(craps) do
+        if crap.RootContainer == Player.Serial then
+            --printItemFn(crap)
+            Player.PickUp(crap.Serial)
+            Player.DropOnGround(crap.Serial)
+            Pause(100)
+        end
+    end
+
+end
+
+function main()
+
+    while true do
+        dropCrap()
+    end
+
+end
+
+main()
+
+
