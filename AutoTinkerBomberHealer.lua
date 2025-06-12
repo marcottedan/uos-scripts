038a2ef4259f57544a1ee9c27c38b9d7f9b3bcab
marcottedan <marcottedan@gmail.com>
2025-06-12 10:32:00 -0400

    Add AutoBomberHealer script Fix a bug in ProcessChok Add UseBandage sub loop

---
+++ AutoTinkerBomberHealer.lua
@@ -0,0 +1,108 @@
+local G = require("Data/Profiles/Scripts/Lib/Global")
+local printItemFn = require("Data/Profiles/Scripts/Lib/Print")
+local findMyItemByNameFn = require("Data/Profiles/Scripts/Lib/FindMyItemByName")
+local findTinkerToolFn = require("Data/Profiles/Scripts/Lib/FindTinkerTool")
+local findLockpickToolFn = require("Data/Profiles/Scripts/Lib/FindLockpickTool")
+local useBandageFn = require("Data/Profiles/Scripts/Lib/UseBandage")
+
+-- 100 Dex should be 10 seconds
+bandageCooldown = 10000
+
+local TrappingChest = true
+local LockpickingChest = true
+
+local trappingSuccess = {
+    "There is no metal here to mine."
+}
+
+local lockpickSuccess = {
+    "You set off a trap!",
+    "This does not appear to be locked."
+}
+
+
+function checkJournal(messages)
+    for _, msg in ipairs(messages) do
+        if Journal.Contains(msg) then
+            return true, msg -- Returns true and the found message
+        end
+    end
+    return false, nil
+end
+
+function bandageSelf()
+    while Player.Hits < Player.HitsMax do
+        if useBandageFn() then
+            -- Wait for 8-12 seconds before checking again (depends on Dex)
+            Pause(bandageCooldown)
+        end
+    end
+end
+
+function lockChest(key, chest)
+    Player.UseObject(key.Serial)
+    Pause(100)
+    Targeting.Target(chest.Serial)
+end
+
+function trapChest(chest)
+    Journal.Clear()
+
+    -- Create Explosion Trap Control Id: 16
+    findTinkerToolFn()
+    if Gumps.WaitForGump(2653346093, 2000) then
+        Pause(100)
+        -- Sends a button click to the gump
+        Gumps.PressButton(2653346093, 16)
+    end
+
+    -- Wait for Tinker crafting the Trap
+    Pause(1500)
+    Targeting.Target(chest.Serial)
+end
+
+function lockpickChest(chest)
+
+    while LockpickingChest do
+        if not findLockpickToolFn() then
+            return
+        end
+        Targeting.WaitForTarget(1000)
+        Targeting.Target(chest.Serial)
+
+        LockpickingChest = not checkJournal(lockpickSuccess)
+    end
+end
+
+function main()
+    Journal.Clear()
+
+    -- Heal yourself to maximum
+    bandageSelf()
+
+    chest = findMyItemByNameFn('Wooden Box')
+    if chest == nil then
+        Messages.Overhead("No Wooden Box", Player.Serial)
+    end
+    key = findMyItemByNameFn('Copper Key')
+    if key == nil then
+        Messages.Overhead("No Copper Key", Player.Serial)
+    end
+
+    -- Trap chest
+    trapChest(chest)
+
+    -- Enable trap
+    lockChest(key, chest)
+
+    -- Lockpick chest to explosion
+    lockpickChest(chest)
+end
+
+while true do
+    TrappingChest = true
+    LockpickingChest = true
+    main()
+end
+
+
